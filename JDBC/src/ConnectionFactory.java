46e2de95221637c78d6b6308c7af69795efb16da
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2022-03-30 10:17:03 -0300

    Pool de conexões

--- JDBC/src/ConnectionFactory.java
+++ JDBC/src/ConnectionFactory.java
@@ -1,13 +1,33 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 // classe de conexao com banco; se eu mudar algo no banco (como senha) eu só preciso alterar aqui
 // toda vez que ela for chamada, é porque queremos a conexao com banco
 
 public class ConnectionFactory {
+	
+	public DataSource dataSource;
+	
+	
+	/* pool de conexões - técnica simples para evitar esse constante "abre-fecha" de conexões com banco de dados é manter um determinado número delas sempre abertas (um "pool" de conexões) 
+	 e simplesmente reusá-las quando necessário, dessa forma você diminui tanto o gasto de recursos da máquina quanto o tempo de resposta da sua aplicação */
+	public ConnectionFactory() { // pool de conexões do mysql
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimeZone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("admin");
+		comboPooledDataSource.setPassword("root");
+		
+		comboPooledDataSource.setMaxPoolSize(15); // carrega pool com 15 conexoes
+		
+		this.dataSource = comboPooledDataSource;
+	}
+	
 	public Connection recuperarConexao() throws SQLException {
-		return  DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimeZone=true&serverTimezone=UTC","admin","root");
+		return this.dataSource.getConnection(); 
 	}
 
 }
---
+++ JDBC/src/TestaPoolConexoes.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaPoolConexoes {
+	public static void main(String[] args) throws SQLException {
+	
+		ConnectionFactory factory = new ConnectionFactory();
+	    
+		for(int i=0;i<20;i++) { // como eu coloquei maximo de pool como 15, ele nao abre 20, abre no máximo 15. As outras  requisições esperarão o processamento das que estão ocupando os espaços já abertos ficar livre
+			factory.recuperarConexao(); // conexao com banco
+			System.out.println("conexão de numero: "+ i);
+		}
+		
+	}
+	 
+
+}
